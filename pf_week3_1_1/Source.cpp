df48fdaed5a04efc38e2b611ac452869d52a6324
SIRIPHICHA MUNSAMER <64010846@KMITL.AC.TH>
2021-08-16 09:08:54 +0700

    Add project files.

---
+++ pf_week3_1_1/Source.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+	float w, h;
+	printf("BMI CALCULATOR\nEnter Weight (kg.) : ");
+	scanf_s("%f", &w);
+	printf("Enter Height (m.) : ");
+	scanf_s("%f", &h);
+
+	float bmi = w / (h * h);
+	printf("%f ", bmi);
+	if (bmi < 18.5)
+		printf("UNDERWEIGHT");
+	else if (bmi < 24.9)
+		printf("NORMAL");
+	else if (bmi < 29.9)
+		printf("OVERWEIGHT");
+	else
+		printf("OBESE");
+	return 0;
+	
+}
